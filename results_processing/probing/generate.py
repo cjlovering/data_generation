a3190d32482e8b02fa726440d595fdd7f1405d49
cjlovering <cjlovering@wpi.edu>
2020-05-12 10:24:58 -0400

    probing input data

--- results_processing/probing/generate.py
+++ results_processing/probing/generate.py
@@ -40,7 +40,8 @@ if __name__ == "__main__":
         help="Input data split.",
     )
     parser.add_argument("--test_size", default=1000, type=int, help="Test size.")
-    parser.add_argument("--train_size", default=1500, type=int, help="Train size.")
+    parser.add_argument("--train_size", default=1000, type=int, help="Train size.")
+    parser.add_argument("--val_size", default=500, type=int, help="Train size.")
     args = parser.parse_args()
 
     with jsonlines.open(f"./outputs/benchmark/{args.split}.jsonl") as reader:
@@ -56,10 +57,21 @@ if __name__ == "__main__":
     train_data_pos = data_pos[args.test_size // 2 :]
     train_data_neg = data_neg[args.test_size // 2 :]
 
-    train_data = random.sample(
-        train_data_pos, math.floor(args.train_size * args.rate)
-    ) + random.sample(train_data_neg, math.ceil(args.train_size * (1 - args.rate)))
+    train_data = (
+        train_data_pos[: math.floor(args.train_size * args.rate)]
+        + train_data_neg[: math.ceil(args.train_size * (1 - args.rate))]
+    )
+    val_data_pos = data_pos[math.floor(args.train_size * args.rate) :]
+    val_data_neg = data_neg[math.ceil(args.train_size * (1 - args.rate)) :]
+    val_data = (
+        val_data_pos[: math.floor(args.val_size * args.rate)]
+        + val_data_neg[: math.ceil(args.val_size * (1 - args.rate))]
+    )
 
+    print(
+        len(val_data),
+        len([d["co-occurs"] for d in val_data if d["co-occurs"]]) / len(val_data),
+    )
     print(
         len(train_data),
         len([d["co-occurs"] for d in train_data if d["co-occurs"]]) / len(train_data),
@@ -68,6 +80,11 @@ if __name__ == "__main__":
         len(test_data),
         len([d["co-occurs"] for d in test_data if d["co-occurs"]]) / len(test_data),
     )
+
+    with jsonlines.open(
+        f"./outputs/probing/val-{args.split}-{args.rate}.jsonl", mode="w"
+    ) as writer:
+        writer.write_all(val_data)
 
     with jsonlines.open(
         f"./outputs/probing/train-{args.split}-{args.rate}.jsonl", mode="w"
